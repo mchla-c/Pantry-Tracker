45a03d5513d33591f6c9ede394d4e2662d130e92
mchla-c <mcastro01204@gmail.com>
2024-08-16 23:25:41 -0700

    base

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,69 +1,208 @@
-import { Box, Stack, Typography } from "@mui/material";
+'use client'
 
-const item = [
-  'tomato',
-  'potato',
-  'onion',
-  'garlic',
-  'ginger',
-  'carrot',
-  'lettuce',
-  'kale',
-  'cucumber'
-]
+import Image from "next/image";
+import {useState, useEffect} from 'react';
+import { firestore } from "@/firebase";
+import {Box, Modal, Typography, Stack, TextField, Button} from '@mui/material'
+import { collection, deleteDoc, doc, getDocs, query, getDoc, setDoc } from 'firebase/firestore'
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Link from "next/link";
+import Layout from "./components/layout";
 
 export default function Home() {
-  return ( 
-    <Box 
-      width="100vw" 
-      height="100vh"
-      display={'flex'} 
-      justifyContent={'center'} 
-      alignItems={'center'}
-      flexDirection={'column'}
-    >
-      <Box
-        border={'1px solid #333'}>
-        <Box
-          width='800px'
-          height='100px'
-          bgcolor={'#ADD8E6'}
-          display={'flex'}
-          justifyContent={'center'}
-          alignItems={'center'}
-        >
-          <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
-            Pantry Items
-          </Typography>
-          
-        </Box>
-        <Stack width='800px' height='300px' spacing={2} overflow={'auto'}>
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const[itemName, setItemName] = useState('')
 
-          {item.map((i) => (
-            <Box 
-              key={i}
-              width="100%"
-              height="300px"
-              display={'flex'}
-              justifyContent={'center'}
-              alignItems={'center'}
-              bgcolor={'#f0f0f0'}
-            >
-              <Typography
-                variant={'h3'}
-                color={'#333'}
-                textAlign={'center'}
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+  }
+
+  const removeItem = async(item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()) {
+      const {quantity} = docSnap.data()
+      if (quantity === 1) {
+        await deleteDoc(docRef)
+      }
+      else {
+        await setDoc(docRef, {quantity: quantity - 1})
+      }
+    }
+
+    await updateInventory()
+  }
+
+  const addItem = async(item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()) {
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity + 1})
+    }
+    else {
+      await setDoc(docRef, {quantity: 1})
+    }
+
+    await updateInventory()
+  }
+
+  useEffect(() => {
+    updateInventory
+  }, [])
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return (
+    <Layout>
+    <Box 
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+    >
+        <Modal
+          open={open}
+          onClose={handleClose}
+        >
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            width={400}
+            bgcolor="white"
+            border="2px solid #000"
+            boxShadow={24}
+            p={4}
+            display="flex"
+            flexDirection="column"
+            gap={3}
+            sx={{
+              transform: 'translate(-50%,-50%)',
+            }}
+          >
+            <Typography variant='h6'>Add Item</Typography>
+            <Stack width="100%" direction="row" spacing={2}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                value={itemName}
+                onChange={(e) => {
+                  setItemName(e.target.value)
+                }}
+              />
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    addItem(itemName)
+                    setItemName('')
+                    handleClose()
+                  }}
+                >
+                  Add
+                </Button>
+
+            </Stack>
+          </Box>
+        </Modal>
+
+        <Button
+          variant="contained"
+          onClick={() => {
+            handleOpen()
+          }}
+        >
+            Add New Item
+        </Button>
+
+        <Box
+          border="1px solid #333"
+        >
+          <Box
+            width="800px"
+            height="100px"
+            bgcolor="#ADD8E6"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="h2" color="#333">Inventory Items</Typography>
+          </Box>
+        <Stack
+          width="800px"
+          height="300px"
+          spacing={2}
+          overflow="auto"
+        >
+          {
+            inventory.map(({name, quantity}) => (
+              <Box
+                key={name}
+                width="100%"
+                minHeight="150px"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                bgcolor="#f0f0f0"
+                padding={5}
               >
-                {/* Capitalize the first letter of each item */
-                  i.charAt(0).toUpperCase() + i.slice(1)
-                }
-              </Typography>
-            </Box>
-          ))}
+                <Typography 
+                  variant='h3' 
+                  color='#333' 
+                  textAlign="center"
+                >
+                  {name.charAt(0).toUpperCase() + name.slice(1)}
+                </Typography>
+
+                <Typography 
+                  variant='h3' 
+                  color='#333' 
+                  textAlign="center"
+                >
+                  {quantity}
+                </Typography>
+                <Stack direction="row" spacing={2}>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      addItem(name)
+                    }}
+                  >
+                    Add
+                  </Button>
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      removeItem(name)
+                    }}
+                  >
+                    Remove
+                  </Button>
+                </Stack>
+              </Box> 
+            ))
+          }
 
         </Stack>
       </Box>
     </Box>
-
-  )
+    </Layout>
+  );
 }
--- pantry/firebase.js
+++ pantry/firebase.js
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAFPux8Oj7aMQ0c-QM7UUY6p7xt6iS1Rco",
-  authDomain: "pantry-tracker-e212b.firebaseapp.com",
-  projectId: "pantry-tracker-e212b",
-  storageBucket: "pantry-tracker-e212b.appspot.com",
-  messagingSenderId: "1094708845920",
-  appId: "1:1094708845920:web:2142b6ccf8f454cc4fd205",
-  measurementId: "G-5RDXJS5GCL"
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: NEXT_PUBLIC_FIREBASE_APP_ID + ".firebaseapp.com",
+  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID ,
+  storageBucket: NEXT_PUBLIC_FIREBASE_APP_ID + ".appspot.com",
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
+  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export {firestore}
